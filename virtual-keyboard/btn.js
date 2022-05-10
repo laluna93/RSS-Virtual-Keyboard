2af7860608557dd02ea37d30faa9041efd4e171f
Anna <Laluna-93@yandex.ru>
2022-05-10 17:02:33 +0400

    feat: add animation

--- virtual-keyboard/btn.js
+++ virtual-keyboard/btn.js
@@ -32,12 +32,11 @@ function createButton(code, caption, className, controller, buttonInfo) {
   };
   btn.onClick = (e) => {
     down(e);
-    up(e);
+    setTimeout(() => up(e), 350)
   };
   const kde = (e) => {
     if (e.code === code) {
       e.preventDefault();
-
       down(e);
     }
   };
